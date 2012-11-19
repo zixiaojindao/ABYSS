124596e5eb82f9e4ed0034406a8c0855204779c3
Sun Zhao <zixiaojindao@gmail.com>
2012-11-19 20:57:45 +0800

    configure all release settings and left two in todo

--- abyss/PopBubbles/PopBubbles.cpp
+++ abyss/PopBubbles/PopBubbles.cpp
@@ -274,15 +274,15 @@ static float getAlignmentIdentity(const Graph& g,
 	vector<int> inDists(nbranches);
 	//add boost:: before cref by Sun Zhao(zixiaojindao@gmail.com)
 	transform(first, last, inDists.begin(),
-			bind(getDistance, boost::cref(g), t, _1));
+			boost::lambda::bind(getDistance, boost::cref(g), t, _1));
 	vector<int> outDists(nbranches);
 	transform(first, last, outDists.begin(),
-			bind(getDistance, boost::cref(g), _1, v));
+			boost::lambda::bind(getDistance, boost::cref(g), _1, v));
 	vector<int> insertLens(nbranches);
 	transform(first, last, insertLens.begin(),
-			bind(getDistance, boost::cref(g), t, _1)
-				+ bind(getLength, &g, _1)
-				+ bind(getDistance, boost::cref(g), _1, v));
+		boost::lambda::bind(getDistance, boost::cref(g), t, _1)
+				+ boost::lambda::bind(getLength, &g, _1)
+				+ boost::lambda::bind(getDistance, boost::cref(g), _1, v));
 
 	int max_in_overlap = -(*min_element(inDists.begin(),
 			inDists.end()));
@@ -302,7 +302,7 @@ static float getAlignmentIdentity(const Graph& g,
 		return max_identity;
 
 	vector<string> seqs(nbranches);
-	transform(first, last, seqs.begin(), bind(getSequence, &g, _1));
+	transform(first, last, seqs.begin(), boost::lambda::bind(getSequence, &g, _1));
 	for (unsigned i = 0; i < seqs.size(); i++) {
 		// Remove the overlapping sequence.
 		int n = seqs[i].size();
